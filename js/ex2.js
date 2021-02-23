2c740b4759d0a7ff86a63d66b8729dcb2fb1bdcb
Mihir Kapur <mihirkapur96@gmail.com>
2021-02-23 12:14:57 -0800

    Final Release

--- js/ex2.js
+++ js/ex2.js
@@ -2,3 +2,19 @@
 Exercise 2 JavaScript code
 */
 console.log ("Mihir's output from Homework 5 Exercise 2");
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+// Using Filter
+const OddNumbers = numbers.filter(x => x % 2 !== 0);
+console.log(OddNumbers);
+
+// Using Filter
+const divisibleby2or5 = numbers.filter (x=> x % 2 == 0 || x % 5 == 0);
+console.log(divisibleby2or5);
+
+// Using Filter and map
+const divisibleby3squared = numbers.filter (x=> x % 3 == 0).map (x=> x ** 2);
+console.log(divisibleby3squared);
+
+// Using filter,map and reduce
+const divibleby5 = numbers.filter (x=> x % 5 == 0).map (x=> x **2).reduce ((acc,value) => acc + value, 0);
+console.log(divibleby5);
--- js/ex3.js
+++ js/ex3.js
@@ -2,3 +2,48 @@
 Exercise 3 JavaScript code
 */
 console.log ("Mihir's output from Homework 5 Exercise 3");
+// Declare student list array
+const studentList = [
+    {
+        firstName: "Allan",
+        lastName: "Able",
+        scores: [95,85,92,98]
+    },
+    {
+        firstName: "Amy",
+        lastName: "Alexander",
+        scores: [80,88,100]
+    },
+    {
+        firstName: "Betty",
+        lastName: "Barns",
+        scores: [70,80,90,100]
+    },
+    {
+        firstName: "Bob",
+        lastName: "Bones",
+        scores: [75,85,95,85]
+    },
+    {
+        firstName: "Cindy",
+        lastName: "Chase",
+        scores: [95,90,92,98]
+    },
+    {
+        firstName: "Charles",
+        lastName: "Chips",
+        scores: [88,99,90]
+    },
+];
+
+const cLastNameResults= studentList.filter (student=> student.lastName.startsWith ("C") && student.firstName.startsWith ("C"))
+    .map(student=> {
+        return {
+            lname: student.lastName,
+            fname: student.firstName,
+            avgGrade: student.scores.reduce((total,score) => total + score,0) / student.scores.length,
+            minScore: student.scores.reduce((min,score) => Math.min(min,score), 100),
+            maxScore: student.scores.reduce((max,score) => Math.max(max,score), 0),
+        };
+    });
+console.log(cLastNameResults);
